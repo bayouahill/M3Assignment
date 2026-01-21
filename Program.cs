e1fa0d544408c98065cd298410f010fe09cc54c3
bayouahill21 <bayouahill21@gmail.com>
2026-01-21 10:31:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Need to allow user to add food items
+//Need to allow user to delete food items
+//Need to print the list of food items
+
+using M3Assignment;
+
+Console.WriteLine("Welcome to the Food Item Manager!");
+Console.WriteLine("Adding or Deleting Food?")
